341e2864a8209f17cdbb5710cea43149bcf47ce7
Radu Zamfir <zamfirr75@gmail.com>
2021-03-31 12:31:06 +0300

    changed the title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Miere Cezara</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
