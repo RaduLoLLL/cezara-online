a648970e60631fe90167aa59bfb08706bc6a78f7
Radu Zamfir <zamfirr75@gmail.com>
2021-04-02 12:15:49 +0300

    bug fixing

--- components/Footer.js
+++ components/Footer.js
@@ -15,7 +15,20 @@ export default function Footer() {
           <h3 className="text-2xl font-light ml-5">+(40) 75 112 0810</h3>
         </div>
         <div className="flex ml-2 lg:ml-0 items-center lg:mx-12 my-10 lg:my-0">
-          <Image src="/email1.svg" width={70} height={70} />
+          <svg
+            className="w-14 h-14 text-yellow-400"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+            />
+          </svg>
           <h3 className="text-2xl font-light ml-5">
             contact@cezaraconstruct.com
           </h3>
