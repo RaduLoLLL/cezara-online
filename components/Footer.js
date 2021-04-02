1c2b00179b1708965edad1e8df119ebbe5adb18d
Radu Zamfir <zamfirr75@gmail.com>
2021-04-02 12:18:55 +0300

    bug fixing

--- components/Footer.js
+++ components/Footer.js
@@ -3,18 +3,18 @@ export default function Footer() {
   return (
     <div className="flex flex-col items-center ">
       <h2 className="text-3xl">Contacteaza-ne</h2>
-      <div className="mt-20 flex flex-col lg:flex-row mb-10">
-        <div className="flex items-center my-10 lg:my-0 lg:mx-12">
+      <div className="mt-20 flex flex-col lg:flex-row mb-10 lg:ml-0">
+        <div className="flex flex-col lg:flex-row items-center my-10 lg:my-0 lg:mx-12">
           <Image src="/house.svg" width={70} height={70} />
           <h3 className="text-2xl font-light ml-5">
             Strada Stadionului Nr. 4A
           </h3>
         </div>
-        <div className="flex items-center lg:mx-12 my-10 lg:my-0">
+        <div className="flex flex-col lg:flex-row items-center lg:mx-12 my-10 lg:my-0">
           <Image src="/phone.svg" width={70} height={70} />
           <h3 className="text-2xl font-light ml-5">+(40) 75 112 0810</h3>
         </div>
-        <div className="flex ml-2 lg:ml-0 items-center lg:mx-12 my-10 lg:my-0">
+        <div className="flex flex-col lg:flex-row lg:ml-0 items-center lg:mx-12 my-10 lg:my-0">
           <svg
             className="w-14 h-14 text-yellow-400"
             fill="none"
