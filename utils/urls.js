7f8d35aa0c422b9fde24c5c59b98432bcc7a242b
Radu Zamfir <zamfirr75@gmail.com>
2021-04-02 09:15:57 +0300

    hardcoded api url

--- utils/urls.js
+++ utils/urls.js
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL = "https://cezara-online-backend.herokuapp.com";
 
 export const fromImageToUrl = (image) => {
   if (!image) {
