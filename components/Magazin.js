b622ce6e5ce66680a6a955f4e627bb05da09bcf4
Radu Zamfir <zamfirr75@gmail.com>
2021-04-02 10:49:29 +0300

    bug fixing

--- components/Magazin.js
+++ components/Magazin.js
@@ -16,7 +16,7 @@ function Magazin({ products }) {
         </h2>
         <div className="flex flex-col lg:flex-row lg:justify-between mt-20 lg:mt-32">
           {products.map((product) => {
-            return <Product product={product} />;
+            return <Product key={product.id} product={product} />;
           })}
         </div>
         <button className="text-3xl mt-20 bg-blue-700 py-2 px-5 lg:px-14 rounded-2xl animate-bounce lg:animate-none text-white">
