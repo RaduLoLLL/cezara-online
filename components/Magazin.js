9ea0479780042b3d1cc1de8cace73816aa755acc
Radu Zamfir <zamfirr75@gmail.com>
2021-04-02 11:55:13 +0300

    bug fixing

--- components/Magazin.js
+++ components/Magazin.js
@@ -6,15 +6,11 @@ function Magazin({ products }) {
   console.log(products);
   return (
     <div className="pb-32 -mx-10">
-      <div className="">
-        <Image src="/wave1.svg" width={1920} height={320} />
-      </div>
       <div className="flex flex-col items-center mt-20">
         <h2 className="text-xl xl:text-5xl lg:text-3xl">Magazinul Nostru</h2>
         <h2 className="text-3xl lg:text-5xl xl:text-7xl font-semibold mt-5 mb-5">
           Produse Favorite
         </h2>
-        {/*
         <div className="flex flex-col lg:flex-row lg:justify-between mt-20 lg:mt-32">
           {products.map((product) => {
             return <Product key={product.id} product={product} />;
@@ -23,7 +19,6 @@ function Magazin({ products }) {
         <button className="text-3xl mt-20 bg-blue-700 py-2 px-5 lg:px-14 rounded-2xl animate-bounce lg:animate-none text-white">
           Vezi toate produsele
         </button>
-        */}
       </div>
     </div>
   );
